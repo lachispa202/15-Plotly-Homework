f1b6c5f906295e97ed68fb417611cf740737cc47
Tobias Judd <tobias_judd@hotmail.com>
2020-11-19 21:26:12 -0600

    Homework Submission

--- Tobias Homework/static/js/app.js
+++ Tobias Homework/static/js/app.js
@@ -1,8 +1,18 @@
 // Belly Button Biodiversity Homework
+// Homework done by Tobias Judd
+
+// referemced class examples Session 15.2 class examples #5, #6, and #7. Plus used class examples
+// from session 15.3 #2, #3, #4, and #5. 
+
+// First approached to take in the Metadata for individual demographics and demographics information. 
+// Afterwards, created the bubble chart and horizontal bar chart based on available information. 
+
+// Attempted to link to update plots / samples by new test subject ID selected. Ran into problems where 
+// data / plots could not be loaded / viewed. 
 
 function init() {
 
-    // TEST Subject ID Information:
+    // TEST Subject ID Information (Step 1 and Step 4):
 
     var selector = d3.select('#selDataset');
     d3.json("./static/js/data/samples.json").then((importedData) => {
@@ -22,7 +32,7 @@ function init() {
 
 // function updateMetadata() {
 
-// Demographic Info:
+// Demographic Info (Step 5):
 d3.json("./static/js/data/samples.json").then((importedData) => {
     var data = importedData
     var metadata = data.metadata;
@@ -50,7 +60,7 @@ d3.json("./static/js/data/samples.json").then((importedData) => {
     var sample_values = result.sample_values;
     var otu_ids = result.otu_ids;
     var otu_labels = result.otu_labels;
-    //  Create the Traces - Bubble Chart
+    //  Create the Traces - Bubble Chart (Step 3)
     var trace = {
         x: otu_ids,
         y: sample_values,
@@ -75,7 +85,7 @@ d3.json("./static/js/data/samples.json").then((importedData) => {
     // Plot the chart to a div tag with id "plot"
     Plotly.newPlot('bubble', chartData, layout);
 
-    //  Create the Traces - Bar Chart
+    //  Create the Traces - Bar Chart (Step 2)
     var trace1 = {
         x: sample_values.slice(0, 10).reverse(),
         y: otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse(),
